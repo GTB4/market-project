5bc4d93b8a5c5035a6e73d4e3b605c6b7cbebf0d
GTB4 <GTB4@users.noreply.github.com>
2025-04-24 16:26:32 +0200

    feat: add argparse for file selection and print only

--- backend/traffic_replay.py
+++ backend/traffic_replay.py
@@ -1,32 +1,43 @@
 from scapy.all import rdpcap, sendp, Ether, IP, UDP
-import time
+import time, argparse, os
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--print_only", action="store_true", help="Use --print_only to see the number of UDP packets that will be sent(will not send any package)")
+    parser.add_argument("--file", type=str, required=True, help="Define the path to the file that will be replayed")
+
+    return parser.parse_args()
 
 def traffic_replay():
-
-    packets = rdpcap(r"junk_data\leather_armor_t6.pcap")
+    args = parse_args()
+    packets = rdpcap(os.path.join("junk_data", args.file))
+    packet_send = 1
     lastindex = None
 
-    for i in range(len(packets)):
-        if i > 0 and packets[i].haslayer("UDP"):
-            if lastindex is not None:
-                delay = float(packets[i].time - packets[lastindex].time)
+    for i, pkt in enumerate(packets):
+        if pkt.haslayer("UDP"):
+            if lastindex is not None and not args.print_only:
+                delay = float(pkt.time - packets[lastindex].time)
                 if delay >= 0.015:
                     time.sleep(delay)
 
-            # Check and modify destination IP for UDP packets
-            pkt = packets[i]
+            # Modify packet destination IP and remove checksums
             if IP in pkt:
-                pkt[IP].dst = "127.0.0.1" 
-                del pkt[IP].chksum  
-                if UDP in pkt:
-                    del pkt[UDP].chksum 
+                pkt[IP].dst = "127.0.0.1"
+                del pkt[IP].chksum
+            if UDP in pkt:
+                del pkt[UDP].chksum
 
-            # Ensure the packet is wrapped in Ethernet if not already
+            # Wrap in Ethernet if missing
             if Ether not in pkt:
                 pkt = Ether() / pkt
 
-            # print(packets[i].summary())  # Print summary of packet
-            sendp(packets[i], iface="Ethernet")
+            if args.print_only:
+                print(f"Packet number = {packet_send}")
+            else:
+                sendp(pkt, iface="Ethernet", verbose=False)
+
+            packet_send += 1
             lastindex = i
 
 if __name__ == "__main__":
